51874ecfd56aa6522e92b389e875a58073ba9cc2
mrkazmi <sdhassaanahmed@gmail.com>
2024-07-04 02:35:49 +0500

    Added speedinsights package in layout.jsx

--- src/screens/Layout.jsx
+++ src/screens/Layout.jsx
@@ -1,12 +1,14 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout() {
   return (
 	<>
 		<Navbar />
 		<Outlet />
+		<SpeedInsights />
 		<Footer />
 	</>
   )
